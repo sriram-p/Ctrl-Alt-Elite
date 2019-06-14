77cde4161690144fa5190f55958106f36b92e78e
Nirmit04 <nirmit.agarwal@nineleaps.com>
2019-06-14 16:15:21 +0530

    d

--- public/renderer/index.js
+++ public/renderer/index.js
@@ -13,3 +13,17 @@ function readme() {
     webview.addEventListener('did-start-loading', loadstart);
     webview.addEventListener('did-stop-loading', loadstop);
 }
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyApws7c76Ndiicm-84XrAXNLP5qZlBTaFM",
+    authDomain: "ctrl-alt-elite-e065c.firebaseapp.com",
+    databaseURL: "https://ctrl-alt-elite-e065c.firebaseio.com",
+    projectId: "ctrl-alt-elite-e065c",
+    storageBucket: "ctrl-alt-elite-e065c.appspot.com",
+    messagingSenderId: "29345198530",
+    appId: "1:29345198530:web:3f278d56fd09788a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
