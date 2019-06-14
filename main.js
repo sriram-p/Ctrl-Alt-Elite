819f8e2d969b96384106655c5bf620b2f4fc35de
Aarshi <kumaraarshianand@nineleaps.com>
2019-06-14 11:55:11 +0530

    init

---
+++ main.js
@@ -0,0 +1,45 @@
+const electron = require('electron')
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+    win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, './public/images/nineleaps_logo.png'),
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+//   win.loadFile(path.join(__dirname, './html/index.html'));
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, './public/html/index.html'),
+    protocol: 'file',
+    slashes: true
+  }));
+
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+      createWindow()
+    }
+  })
+
+
+app.on('ready', createWindow)
---
+++ public/renderer/index.js
@@ -0,0 +1,3 @@
+function readme() {
+    alert('Hi electron! Its us Ctrl Alt Elite.');
+}
