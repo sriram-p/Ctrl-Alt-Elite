bc8f29833169fbdba9a688485cebefed08ce9939
mrigendraRai <mrigendra@nineleaps.com>
2019-06-14 14:39:56 +0530

    Login page

--- public/renderer/index.js
+++ public/renderer/index.js
@@ -1,3 +1,15 @@
-function readme() {
-    alert('Hi electron! Its us Ctrl Alt Elite.');
-}
+// function readme() {
+//     alert('Hi electron! Its us Ctrl Alt Elite.');
+// }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
